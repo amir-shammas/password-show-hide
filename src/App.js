94a25139f0cd2335e6fc452e7eaf6b5a560446be
ShammasAmir <amirshammas@gmail.com>
2024-09-10 04:03:42 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { Icon } from 'react-icons-kit';
+import {eye} from 'react-icons-kit/feather/eye';
+import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+
+
+
+function App() {
+
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(eyeOff);
+
+  function handleClick(){
+    // console.log("clicked !");
+    if(type === "password"){
+      setType("text");
+      setIcon(eye);
+    }else{
+      setType("password");
+      setIcon(eyeOff);
+    }
+  }
+
+  return (
+    <>
+      <div className="wrapper">
+        <div className="input-fields">
+          <span onClick={handleClick} ><Icon icon={icon} size={25} /></span>
+          <input type={type} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
